4e4c065035aaa9c574cd17d58e65e26ec4311031
Md Imran <xsmartarman123@gmail.com>
2025-12-07 19:03:20 +0530

    initial commit

---
+++ vite-project/src/component/App.jsx
@@ -0,0 +1,149 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
+import { FaArrowRight, FaHeart } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
+import BaseUrl from "../Axios";
+import { FaCopy } from "react-icons/fa6";
+import { FaExternalLinkAlt, FaShareAlt } from "react-icons/fa";
+import { ImStatsDots } from "react-icons/im";
+import { FaInstagram } from "react-icons/fa6";
+
+function Home() {
+  const [input, setInput] = useState("");
+  const [url, setUrl] = useState("");
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    if (!input.trim()) {
+      toast.error("Please Enter a Valid Url", { duration: 1500 });
+      return;
+    }
+    try {
+      const Result = await BaseUrl.post("/home", { url: input });
+      setUrl(Result.data);
+      console.log(Result.data);
+    } catch (error) {
+      toast.error("Invalid URL format.Example: https://www.example.com", {
+        duration: 1500,
+      });
+      console.log(error);
+      setInput("");
+      return;
+    }
+  };
+
+  const HandleRedirect = async () => {
+    try {
+      if (url && url.id) {
+        const res = await BaseUrl.get(`/${url.id}`, {
+          // redirect: "follow",
+        });
+
+        if (res.data && res.data.redirectUrl) {
+          window.open(res.data.redirectUrl, "_blank");
+        } else {
+          console.log("error is hpping here");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const EmptyText = () => {
+    setInput("");
+    setUrl("");
+  };
+  const CopyLink = async () => {
+    if (url && url.id) {
+      const shortUrl = `http://localhost:8000/${url.id}`;
+      navigator.clipboard.writeText(shortUrl);
+      toast.success("URL Copied!");
+    }
+  };
+  return (
+    <>
+      <div className="min-h-screen bg-purple-200 flex flex-col items-center ">
+        <Toaster position="top-center font-semibold" />
+        <h1 className="text-2xl md:text-[2.34rem] font-semibold flex gap-[5px] mt-[20px]">
+          URL SHORTNER{" "}
+          <FaLink className="mt-[5px] md:mt-[10px] cursor-pointer text-blue-900" />{" "}
+        </h1>
+        <div className="h-[200px] gap-[13px] flex flex-col w-full  md:w-[700px] mt-[12px] bg-slate-200 rounded-[12px] p-4">
+          <label className="text-[1.43rem]">Paste your long link here</label>
+
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="w-[90%] h-[50px] rounded-[8px] ml-0 md:ml-[40px] focus:outline-none focus:border-blue-800 border-[0.18rem] text-center text-[1.2rem] "
+            placeholder="http//examples.com/my-long-url"
+          />
+
+          <button
+            onClick={HandleSubmit}
+            className="flex items-center justify-center gap-2 w-[180px] rounded-[17px] h-[50px] bg-blue-600 text-white text-[1.23rem] cursor-pointer"
+          >
+            Get your link
+            <FaArrowRight />
+          </button>
+        </div>
+        {url && (
+          <div className="  rounded-[10px] mt-[20px] flex flex-col  items-center  bg-slate-500">
+            <h2
+              onClick={HandleRedirect}
+              className="cursor-pointer pt-[4px] hover:underline text-[1.63rem] text-blue-800 font-bold"
+            >
+              {url ? `http://short.ly/${url.id}` : ""}
+            </h2>
+            <div className=" h-[35px] flex gap-[30px] mt-[10px] ml-[10px] cursor-pointer">
+              <div className="   text-[1.3rem]   ">
+                <FaCopy
+                  onClick={CopyLink}
+                  className="text-black-500 hover:text-red-800 font-bold   transition duration-300 transform hover:scale-155"
+                />
+              </div>
+              <div className="   text-[1.3rem] cursor-pointer">
+                <FaShareAlt className="text-black hover:text-purple-800 transition duration-300 transform hover:scale-155" />
+              </div>
+              <div className="  text-[1.3rem] cursor-pointer">
+                <FaExternalLinkAlt
+                  className="text-black-500 hover:text-blue-800 transition duration-300 transform hover:scale-155"
+                  onClick={HandleRedirect}
+                />
+              </div>
+              <div className="  text-[1.3rem] cursor-pointer">
+                <Link to={`/detalis/${url.id}`}>
+                  <ImStatsDots className="text-black-500 hover:text-blue-800 transition duration-300 transform hover:scale-155" />
+                </Link>
+              </div>
+            </div>
+            <button
+              onClick={EmptyText}
+              className="w-full md:w-[200px] mb-[8px] h-[33px] rounded-[15px]  bg-purple-400 text-[1.2rem] cursor-pointer hover:bg-purple-600 "
+            >
+              Shorten Another Link
+            </button>
+          </div>
+        )}
+        <div className="fixed bottom-0 w-full md:w-auto flex justify-center md:justify-end p-4 md:right-5">
+          <h1 className="flex items-center gap-2 md:gap-1 text-sm md:text-base">
+            Web Dev by
+            <a
+              href="https://www.instagram.com/only_arman18/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 flex  hover:underline hover:scale-105 transition-transform duration-300 "
+            >
+              <FaInstagram className="mt-[5px] ml-[5px]  cursor-pointer hover: text-red-800" />{" "}
+              @Only_Arman18
+            </a>
+          </h1>
+          <FaHeart className="pb-[1px] pl-[5px] text-[1.45rem] cursor-pointer text-red-600 hover:text-red-900 hover:scale-125 transition-transform duration-300  " />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ vite-project/src/component/New.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import BaseUrl from "../Axios";
+import { MdAdsClick } from "react-icons/md";
+import { FaHeart, FaInstagram } from "react-icons/fa";
+
+const New = () => {
+  const { shortId } = useParams();
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const fetchResponse = async () => {
+      const res = await BaseUrl.get(`/detalis/${shortId}`);
+      setData(res.data);
+    };
+
+    fetchResponse();
+  }, [shortId]);
+  return (
+    <div className="min-h-screen bg-purple-200  flex flex-col  items-center">
+      {data ? (
+        <>
+          <div className="my-10">
+            <h1 className="text-4xl font-bold text-red-400 flex ">
+              Total Clicks{" "}
+              <MdAdsClick className=" mt-[6px] text-blue-400 mx-2 animate-pulse " />
+              : {data.totalClick}
+            </h1>
+
+            <div className="my-4 flex flex-col bg-slate-300 rounded-lg px-2 items-center">
+              <h2 className="text-3xl">Detalis Of Clicks </h2>
+
+              {data.detalis && data.detalis.length > 0 ? (
+                data.detalis.map((item, index) => (
+                  <div className="flex space-x-4 flex   ">
+                    <div className="text-xl" key={index}>
+                      {index+1}.
+                    </div>
+
+                    <div className="text-2xl">
+                      {new Date(item.timestamp).toLocaleString()}
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <li>No visit history yet</li>
+              )}
+            </div>
+          </div>
+        </>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+      <div className="flex justify-center ">
+        <h1 className="flex text-xl font-semibold">
+          web dev by{" "}
+          <a
+            href="https://www.instagram.com/only_arman18/"
+            className="  text-blue-400 hover:underline hover:text-blue-800 flex text-[14px] my-[6px] hover:scale-105 transition-transform duration-300"
+          >
+            <FaInstagram className=" mx-[5px] my-[5px] text-[0.8rem] cursor-pointer hover: text-red-800" />{" "}
+            @Only_Arman18
+          </a>
+        </h1>
+        <FaHeart className="mt-[4px] pl-[5px] text-[1.45rem] cursor-pointer text-red-600 hover:text-red-900 hover:scale-125 transition-transform duration-300  " />
+      </div>
+    </div>
+  );
+};
+
+export default New;
